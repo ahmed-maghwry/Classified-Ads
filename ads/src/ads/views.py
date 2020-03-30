ab59598d3858ea07d08a77b1e766fd012b694230
[Ahmed Maghwry] <[ahmed_mag22@YAHOO.COM]>
2020-03-30 08:34:25 -0700

    delet test

--- ads/src/ads/views.py
+++ ads/src/ads/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from .models import *
 
 # Create your views here.
 
-def test (request):
-    return render (request , 'index.html' , {})
+def all_ads(request):
+    ad=

a7ebc6ae9820a61c4c22a8c1da9e97bc504e6537
[Ahmed Maghwry] <[ahmed_mag22@YAHOO.COM]>
2020-03-30 06:51:53 -0700

    add static and media file Settings

--- ads/src/project/urls.py
+++ ads/src/project/urls.py
@@ -12,10 +12,20 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from ads import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.test, name="ahmed"),
+
+
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

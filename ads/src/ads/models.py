f3ea83782fbeb1d9401deeb47ab21c879446a502
[Ahmed Maghwry] <[ahmed_mag22@YAHOO.COM]>
2020-03-30 05:43:55 -0700

    add ads model's filds and migrate and super user

--- ads/src/ads/models.py
+++ ads/src/ads/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
-ffffv
+class ads(models.Model):
+    options =( 
+    ("ex", "ex"), 
+    ("Sale", "Sale"), 
+    ("free", "free"), 
+    ) 
+
+    user = models.ForeignKey(User, blank=True , null=True, on_delete=models.CASCADE)
+    # slug= models.SlugField(default='' , null = True , blank= True)
+    title =models.CharField(max_length=80)
+    description = models.TextField(max_length=500,default='')
+    create_date = models.DateTimeField(default=timezone.now)
+    active = models.BooleanField(default=True)
+    view =models.IntegerField(default=0)
+    ad_option = models.CharField(max_length=4, choices=options , default='ex')
+
+
+    class Meta:
+        ordering = ['-view']
+    def __str__(self):
+        return self.title

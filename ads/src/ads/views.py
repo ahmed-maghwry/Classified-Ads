519cb7d1fcfab4f01854cb14faaed8d77c82dca4
[Ahmed Maghwry] <[ahmed_mag22@YAHOO.COM]>
2020-03-30 07:13:47 -0700

    add home html and css and icon

--- ads/src/ads/views.py
+++ ads/src/ads/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+def test (request):
+    return render (request , 'index.html' , {})

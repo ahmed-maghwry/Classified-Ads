9df54741da80a59eea2c3dc0f665c46de26f5516
[Ahmed Maghwry] <[ahmed_mag22@YAHOO.COM]>
2020-04-08 04:53:36 -0700

    try to add car categories

--- ads/src/ads/admin.py
+++ ads/src/ads/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import ads
+from .models import ads , car_cat
 # Register your models here.
 admin.site.register(ads)
+admin.site.register(car_cat)
+
--- ads/src/ads/models.py
+++ ads/src/ads/models.py
@@ -20,8 +20,34 @@ class ads(models.Model):
     view =models.IntegerField(default=0)
     ad_option = models.CharField(max_length=4, choices=options , default='ex')
     img=models.ImageField(upload_to='post_img/' , default="img/defu.png")
+    name_of_who=models.CharField(max_length=15 , null=True,blank=True)
+    adress=models.CharField(max_length=80 , null=True,blank=True)
+    mobile =models.PositiveSmallIntegerField( default=1114796307 , null=True,blank=True,)
+    email=models.EmailField(default="ahmed_mag22@yahoo.com" , null=True,blank=True)
+
+
 
     class Meta:
         ordering = ['-view']
     def __str__(self):
         return self.title
+
+
+class car_cat(models.Model):
+    cat_name=models.CharField ( max_length=15)
+    cat_main=models.ForeignKey ( 'self' , related_name='prod_category_set', 
+                                limit_choices_to={'cat_main__isnull':True},
+                                on_delete=models.CASCADE,blank=True,null=True)
+    # cat_Sub=models.ForeignKey ( 'self' , related_name='prod_sub_category_set',
+    #                             limit_choices_to={'cat_Sub__isnull':True ,
+    #                                               'cat_main__isnull':False }
+    #                             ,on_delete=models.CASCADE,blank=True,null=True)
+    # cat_desc=models.TextField()
+
+
+                                #  limit_choices_to={'cat_main__isnull':True
+                                #      ,'cat_Sub__isnull':True}
+                                #  , 
+
+    def __str__(self):
+        return self.cat_name

6285f174d560a1e994c66f0ef363d40c269a9393
[Ahmed Maghwry] <[ahmed_mag22@YAHOO.COM]>
2020-04-20 04:12:14 -0700

    add comment to change_form Function

--- ads/src/ads/views.py
+++ ads/src/ads/views.py
@@ -5,9 +5,8 @@ from . forms import carf , mobilef , adsform , bbf
 
 
 
-cat="Ahmed"
+cat=""# Empty variable use like signal and i think it is not important but i'm afraid to delete it
 # Create your views here.
-
 def all_ads(request):
     ads_all=ads.objects.all()
     context = {
@@ -21,33 +20,43 @@ def ads_detail(request , id):
         'detail':detail ,
     }
     return render(request,'detail.html', context)
-
     #########################################################################
+    # THIS Function USE TO CHANGE THE EXTENSION FORMS FOR VARIABLE OPTIONS 
+    # LIKE SOME FIELDS FOR CARS ADS AND ANTHER FOR MOBILE ADS AND FOR JOB ADS
+        # FOR MORE DETAILS {CARS FIELDS :Model , Model Year ,Kilometers  , 
+        # Transmission Type ,Body Type ,Engine Capacity (CC)}
+    # FOR MORE DETAILS {MOPILE FIELDS :Brand , RAM , processor Model }
+    ###############
+    # HOW IT WORK #
+    ###############
+    # 1- make ajax Function to return the id of chosen category if we have it
+    #    and that to selected The right fields for the chosen category 
+    #    If the identifier is 8, then this means that the user chooses cars ads  
+    #    and so on for each category number 
+    # 2-and if the category doesn't have special fields then return signalf context as zero
+    # finally  if signalf context == zero there are no special fields for this ad
+
+
 def change_form (request):
-    main_ff = request.GET.get('mainff')
-    if main_ff == "8" :
+    main_form_id = request.GET.get('main_form_id')
+    if main_form_id == "8" :
         cat=bbf()
-    elif main_ff == "23" :
+    elif main_form_id == "23" :
         cat=mobilef()
     else :
-        cat="Ahmed"
+        cat="Ahmed" # Empty variable use like signal and i think it is not important but i'm afraid to delete it
+# Create your views here.
     context2 ={
-
         'cat':cat,
-        'signalf' :0
+        'signalf' :0  #variable use like signal to make me actually know is there main_form_id variable come with response or not
     }
-    
     return render (request , 'change_form.html' , context2)
-
-
     #########################################################################
 def creat_ads(request):
     signalf=0
-    
     if request.method =='POST':
         print (" request is post")
         form = adsform(request.POST ,request.FILES)
-        
         if form.data['main'] == "8" :
             print ("main = 8")
             catff = bbf (request.POST , request.FILES)
@@ -58,7 +67,6 @@ def creat_ads(request):
             catff = mobilef (request.POST , request.FILES)  
             catf22=mobilef()
             signalf=1
-
         else : 
             print ("no main response")
             pass
@@ -71,8 +79,6 @@ def creat_ads(request):
                 form.save()
                 catff.save()
                 return redirect('/')
-
-            
             else:
                 print ("return one of two forms is not valid")
                 context = {
@@ -96,23 +102,19 @@ def creat_ads(request):
     else:
         print (" not POST request")
         form= adsform() 
-
     try:
         context = {
             'form': form ,
             'catff': catff,
             'signalf': signalf,
         }
-
     except :
         context = {
         'form': form ,
         'signalf': signalf,
     }
-
     return render (request , 'creat.html' , context)
     #########################################################################
-
 def load_sub(request):
     main_idt = request.GET.get('main')
     sub_idt = request.GET.get('sub')
@@ -122,21 +124,13 @@ def load_sub(request):
     last=[]
     if sub_idt :
         end = catugry.objects.filter( main_id=main_idt ,sub_id=sub_idt ,end_id=None).order_by('name')
-        sub=[]
-    
+        sub=[]  
     else: pass
-        # sub =  catugry.objects.none()        
-        # end =  catugry.objects.none()
-        # last = catugry.objects.none()
     if end_idt:
         last = catugry.objects.filter( main_id=main_idt ,sub_id=sub_idt ,end_id=end_idt).order_by('name')
         sub=[]
         end=[]
-
     else: pass
-        # end = catugry.objects.none()
-        # last = catugry.objects.none()
-        # sub = catugry.objects.none()
     context={
         'sub': sub ,
         'end': end ,

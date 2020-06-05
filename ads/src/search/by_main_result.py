52ee6a09dfc9a7e03ebf833d93bc7c6fbc4dca6e
[Ahmed Maghwry] <[ahmed_mag22@YAHOO.COM]>
2020-06-05 10:31:56 +0200

    search all in one def

--- ads/src/search/by_main_result.py
+++ ads/src/search/by_main_result.py
@@ -11,7 +11,7 @@ def change_form_search (request):
     forms_={
         '44' : car_search() , '46' : motorcycles_search() , '47' : car_spare_parts_search() ,
         '49' : Boats_search() , '48' : heavy_trucks_search() , '51' : mobile_phones_search() ,
-        '50' : mobile_accessories_search()   
+        '52' : mobile_accessories_search()   
     }
     if sub_form_id in forms_ :
         main_form_search=forms_[sub_form_id]  
@@ -31,53 +31,65 @@ def check_is_number(number):
     else :return False
 
 def by_main_result(request):
+    main_id = request.GET.get('mainId')
     search_db_id = request.GET.get('sub')
     db_list={
-            '':'no_form' , '44' : 'car', '46' : 'motorcycles' , '47' : 'car_spare_parts' ,
-            '49' : 'Boats' , '48' : 'heavy_trucks' , '51' : 'mobile_phones' ,
-            '50' : 'mobile_accessories'   
+            '':'no_form' , '44' : 'db_car', '46' : 'db_motorcycles' , '47' : 'db_car_spare_parts' ,
+            '49' : 'db_Boats' , '48' : 'db_heavy_trucks' , '51' : 'db_mobile_phones' ,
+            '52' : 'db_mobile_accessories'   
         }
     if check_is_number(search_db_id)==True :
         db_search=db_list[search_db_id]
-    main_id_=43
     general_list=['sub' , 'end' ,'last']
     price_list=['to_Price', 'from_Price' ]
     signal=0
-    if request.is_ajax():
-        main_catugry_q=ads.objects.filter(main_id=main_id_ , sub_id=search_db_id)
+    if main_id == None and search_db_id !="" and search_db_id != None :
+        print("main none")
         print(search_db_id)
-        print(main_catugry_q)
-        for search_key in request.GET :
-            search_value=request.GET.get(search_key)
-            if search_value != "" and search_value != None :
+        if request.is_ajax() :
+            main_catugry_q=ads.objects.filter( sub_id=search_db_id)
+
+            for search_key in request.GET :
+                
                 search_value=request.GET.get(search_key)
-                if search_key in general_list :
-                    signal=1
-                    search_var={search_key : search_value}
-                    main_catugry_q=main_catugry_q.filter( ** search_var )
-                elif  search_key not in general_list and search_key not in price_list and search_value != 'on' :
-                    signal=1
-                    exe_search_var="{}__{}__iexact".format(db_search ,search_key)
-                    diction_search_var={exe_search_var:search_value}
-                    main_catugry_q=main_catugry_q.filter(** diction_search_var )
-                elif  search_key not in general_list and search_value == 'on' :
-                    signal=1
-                    bool_search_var="{}__{}__iexact".format(db_search ,search_key)
-                    diction_bool_search_var={bool_search_var:'1'}
-                    main_catugry_q=main_catugry_q.filter(** diction_bool_search_var )
-                elif  search_key in price_list and check_is_number(search_value) == True  :
-                    if search_key == 'from_Price'  :
+            
+                if search_value != "" and search_value != None :
+                    search_value=request.GET.get(search_key)
+                    if search_key in general_list :
                         signal=1
-                        bool_search_var="{}__{}__gte".format(db_search , 'Price')
-                        diction_bool_search_var={bool_search_var : search_value}
-                        main_catugry_q=main_catugry_q.filter(** diction_bool_search_var )
-                    elif search_key == 'to_Price' :
+                        search_var={search_key : search_value}
+                        main_catugry_q=main_catugry_q.filter( ** search_var )
+                    elif  search_key not in general_list and search_key not in price_list and search_value != 'on' :
                         signal=1
-                        bool_search_var="{}__{}__lt".format(db_search , 'Price')
-                        diction_bool_search_var={bool_search_var : search_value}
+                        exe_search_var="{}__{}__iexact".format(db_search ,search_key)
+                        diction_search_var={exe_search_var:search_value}
+                        main_catugry_q=main_catugry_q.filter(** diction_search_var )
+                    elif  search_key not in general_list and search_value == 'on' :
+                        signal=1
+                        bool_search_var="{}__{}__iexact".format(db_search ,search_key)
+                        diction_bool_search_var={bool_search_var:1}
                         main_catugry_q=main_catugry_q.filter(** diction_bool_search_var )
-        if signal == 0 :
-            main_catugry_q=ads.objects.filter(main_id=main_id_).order_by('-create_date')
+                    elif  search_key in price_list and check_is_number(search_value) == True  :
+                        if search_key == 'from_Price'  :
+                            signal=1
+                            bool_search_var="{}__{}__gte".format(db_search , 'Price')
+                            diction_bool_search_var={bool_search_var : search_value}
+                            main_catugry_q=main_catugry_q.filter(** diction_bool_search_var )
+                        elif search_key == 'to_Price' :
+                            signal=1
+                            bool_search_var="{}__{}__lt".format(db_search , 'Price')
+                            diction_bool_search_var={bool_search_var : search_value}
+                            main_catugry_q=main_catugry_q.filter(** diction_bool_search_var )
+            if signal == 0 :
+                main_catugry_q=ads.objects.filter(main_id=main_id).order_by('-create_date')
+                pass
+
+        else:pass
+    
+    else:
+
+        print("main namper")
+        main_catugry_q=ads.objects.filter(main_id=main_id).order_by('-create_date')
     context = {'main_catugry_q' : main_catugry_q ,}
     return render(request, 'by_main_result.html',context)
 
--- ads/src/search/urls.py
+++ ads/src/search/urls.py
@@ -8,30 +8,8 @@ app_name = 'search'
 urlpatterns = [
     # url(r'^(?P<catugry_1>[\w-]+)/$',views.by_catugry, name='by_catugry') ,
     url(r'^$', views.by_catugry, name='by_catugry'),# <-- this one here #
-    url(r'^main/properties/(?P<main_id_>\d+)$', views.by_main_properties , name='by_main_properties'),
-    url(r'^main/Sports/(?P<main_id_>\d+)$', views.by_main_Sports , name='by_main_Sports'),
-    url(r'^main/Babies/(?P<main_id_>\d+)$', views.by_main_Babies , name='by_main_Babies'),
-    url(r'^main/Vehicles/(?P<main_id_>\d+)$', views.by_main_Vehicles , name='by_main_Vehicles'),
+    url(r'^main/(?P<main_id_>\d+)$', views.by_main_all , name='by_main_all'),
     url(r'main/Vehicles/ajax/', by_main_result.by_main_result , name='by_main_result'),
     url(r'main/change_form/ajax/', by_main_result.change_form_search , name='change_form_search'),
-
-    url(r'^main/Mobile/(?P<main_id_>\d+)$', views.by_main_Mobile , name='by_main_Mobile'),
-    url(r'^main/clothes/(?P<main_id_>\d+)$', views.by_main_clothes , name='by_main_clothes'),
-    url(r'^main/Appliances/(?P<main_id_>\d+)$', views.by_main_Appliances , name='by_main_Appliances'),
-    url(r'^main/Pets/(?P<main_id_>\d+)$', views.by_main_Pets , name='by_main_Pets'),
-    url(r'^main/Electronic/(?P<main_id_>\d+)$', views.by_main_Electronic , name='by_main_Electronic'),
-    url(r'^main/Services/(?P<main_id_>\d+)$', views.by_main_Services , name='by_main_Services'),
-    url(r'^main/Furniture/(?P<main_id_>\d+)$', views.by_main_Furniture , name='by_main_Furniture'),
-
-
-    # url(r'^$',views.all_ads, name='all_ads') ,
-    # url(r'^(?P<id>\d+)$', views.ads_detail , name='ads_detail'),
-    # url(r'^creat$', views.creat_ads , name='creat_ads'),
-    # url('ajax/load-cities/', views.load_sub, name='load_sub'),# <-- this one here #
-    # url('ajax/change_form/', views.change_form, name='change_form'),   # <-- this one here
-    # url(r'^(?P<id>\d+)/edit$', edit.edit_ads, name='edit_ads'),
-    # url(r'^$',views.all_post, name='all_post' ) ,
-    # url(r'^del/(?P<id>\d+)$', views.del_post, name='del_post'),
-    # url(r'^test$', views.test, name='test'),
 
 ]

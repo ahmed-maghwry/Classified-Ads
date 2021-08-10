cf9d955633fdd465fadf5c4cd5fd5dca40466463
[Ahmed Maghwry] <[ahmed_mag22@YAHOO.COM]>
2021-08-10 05:54:27 +0200

    fix favoret

--- ads/src/ads/urls.py
+++ ads/src/ads/urls.py
@@ -12,11 +12,6 @@ urlpatterns = [
     url('ajax/change_form/', views.change_form, name='change_form'),   # <-- this one here
     url(r'^(?P<id>\d+)/edit$', edit.edit_ads, name='edit_ads'),
 
-
-
-
-
-
     # url(r'^$',views.all_post, name='all_post' ) ,
     # url(r'^del/(?P<id>\d+)$', views.del_post, name='del_post'),
     # url(r'^test$', views.test, name='test'),
